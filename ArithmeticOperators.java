150a83d6dfedf1392a3db093e7d19428b504372b
Deltong918 <tongdelong918@gamil.com>
2024-04-11 10:52:59 +0900

    no message

---
+++ ArithmeticOperators.java
@@ -0,0 +1,22 @@
+public class ArithmeticOperators{
+	public static void main(String[] args){
+		int A = 10;
+		double B = ++A;
+		int C = 1300;
+		C++;
+		int D = C;
+//		C++;
+//		System.out.println("A + B = " + (A + B));
+//		System.out.println("A + D = " + A + D);
+//		System.out.println("C - B = " + (C - B));
+//		System.out.println("D / B = " + (D / B));
+//		System.out.println("A * C = " + (A * C));
+//		System.out.println("C % D = " + (C % D));
+//		System.out.println("--C = " + (--C));
+//		System.out.println("++B = " + (++B));
+//		System.out.println("--A = " + (--A));
+//		System.out.println("D++ = " + (D++));
+		System.out.println(B);
+		System.out.println(D);
+	}
+}
